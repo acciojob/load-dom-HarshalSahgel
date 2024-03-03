93aa31643dad7fa7d63a0b2f72b02318b4fa9e54
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-03-04 00:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
  
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("onload", function(){
 	console.log("DOM load success");
 });
